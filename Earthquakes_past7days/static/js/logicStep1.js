66d83b162c8ed2b309adef2d06673861348f778c
jenny <liang@Liangs-iMac.local>
2021-12-09 10:11:40 -0800

    GeoJSON from USGS.gov

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -162,33 +162,38 @@ let map = L.map('mapid', {
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// Accessing the Toronto neighborhoods GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/Jenny16x/Mapping_Earthquakes/Simple_Leaflet_Map/torontoNeighborhoods.json";
-// let airportData = "https://raw.githubusercontent.com/Jenny16x/Mapping_Earthquakes/Simple_Leaflet_Map/majorAirports.json";
-let myStyle = {
-  color: "blue",
-  weight : 1
-};
-// // Grabbing our GeoJSON data.
-// d3.json(airportData).then(function(data) {
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
+});
+// // Accessing the Toronto neighborhoods GeoJSON URL.
+// let torontoHoods = "https://raw.githubusercontent.com/Jenny16x/Mapping_Earthquakes/Simple_Leaflet_Map/torontoNeighborhoods.json";
+// // let airportData = "https://raw.githubusercontent.com/Jenny16x/Mapping_Earthquakes/Simple_Leaflet_Map/majorAirports.json";
+// let myStyle = {
+//   color: "blue",
+//   weight : 1
+// };
+// // // Grabbing our GeoJSON data.
+// // d3.json(airportData).then(function(data) {
+// //   console.log(data);
+// // // Creating a GeoJSON layer with the retrieved data.
+// //   L.geoJson(data).addTo(map);
+// // });
+
+// d3.json(torontoHoods).then(function(data) {
 //   console.log(data);
 // // Creating a GeoJSON layer with the retrieved data.
-//   L.geoJson(data).addTo(map);
+// L.geoJson(data
+//   , {
+//       onEachFeature: function(feature, layer) {
+//           style : myStyle
+//           console.log(layer);
+//           layer.bindPopup("<h3> Neighborhood: " + feature.properties.AREA_NAME + 
+//           // "</h3> <hr><h3> Airport name: "
+//           //      + feature.properties.name + 
+//                "</h3>");
+//       }
+//   }
+//   ).addTo(map);
 // });
-
-d3.json(torontoHoods).then(function(data) {
-  console.log(data);
-// Creating a GeoJSON layer with the retrieved data.
-L.geoJson(data
-  , {
-      onEachFeature: function(feature, layer) {
-          style : myStyle
-          console.log(layer);
-          layer.bindPopup("<h3> Neighborhood: " + feature.properties.AREA_NAME + 
-          // "</h3> <hr><h3> Airport name: "
-          //      + feature.properties.name + 
-               "</h3>");
-      }
-  }
-  ).addTo(map);
-});
